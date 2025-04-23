64dde530f98d796102418b4c7f7032ec0be096e4
pavitrakumargupta <pavitragupta021@gmail.com>
2025-04-24 01:18:07 +0530

    change

--- backend/uploadFileHandler.js
+++ backend/uploadFileHandler.js
@@ -12,7 +12,6 @@ dotenv.config();
 
 const AWS_ACCESS_KEY_ID=process.env.AWS_ACCESS_KEY_ID
 const AWS_SECRET_ACCESS_KEY=process.env.AWS_SECRET_ACCESS_KEY
-console.log(process.env.AWS_ACCESS_KEY_ID,process.env.AWS_SECRET_ACCESS_KEY);
 
 
 const s3 = new S3({
